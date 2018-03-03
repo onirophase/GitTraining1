f7e3f6477297c66ecac5eb8f64f18ec977cb705e
onirophase <onirophase@hotmail.fr>
2018-03-03 02:19:03 +0100

    HS

---
+++ src/gestion/Hyper.java
@@ -0,0 +1,9 @@
+package gestion;
+
+public class Hyper {
+
+	public Hyper() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/gestion/Super.java
@@ -0,0 +1,9 @@
+package gestion;
+
+public class Super {
+
+	public Super() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
