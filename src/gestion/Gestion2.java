f4c908e7def7b3e42312da2d770f28707f4e509b
onirophase <onirophase@hotmail.fr>
2018-03-03 02:07:59 +0100

    G2

---
+++ src/gestion/Gestion2.java
@@ -0,0 +1,16 @@
+package gestion;
+
+public class Gestion2 {
+
+	private String points;
+
+	public Gestion2() {
+		points = new String("NSEO");
+	}
+
+	public static void main(String[] args) {
+		Gestion2 g = new Gestion2();
+
+	}
+
+}
